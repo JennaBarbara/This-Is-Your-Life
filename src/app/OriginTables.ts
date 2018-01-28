8f23b6d93e69280acb3463ff96189c0c7c488e43
Jenna Barbara Christensen <jennabarbara@gmail.com>
2018-01-27 19:04:03 -0500

    origin component and service

--- src/app/OriginTables.ts
+++ src/app/OriginTables.ts
@@ -75,13 +75,13 @@ export const Occupations = {
   "96-100": "Soldier"
 };
 export const Races = {
-  "01-40": "Human",
+  "1-40": "Human",
   "41-50": "Dwarf",
   "51-60": "Elf",
   "61-70": "Halfling",
   "71-75": "Dragonborn",
   "76-80": "Gnome",
-  "31-35": "Half-elf",
+  "81-85": "Half-elf",
   "86-90": "Half-orc",
   "9l-95": "Tiefling",
   "96-100": "DM's choice" //TODO something weird with DM's choice. it flickers out on screen
